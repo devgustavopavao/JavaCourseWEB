a9a6f81971621afed4d8b0e29a84dbfc739d6076
gustavo pavao <gusta.pavao@gmail.com>
2023-02-27 09:53:15 -0300

    testing commit

--- JavaWebCourse/src/com/devgusta/HelloWorld.java
+++ JavaWebCourse/src/com/devgusta/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		String nome = "Gustavo";
 		int idade = 22;
+		String sobrenome = "Pavao";
 		System.out.println("Hello,World! by "+nome + " idade: "+idade);
 		
 	}
